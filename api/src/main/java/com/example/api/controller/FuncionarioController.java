638dd3392e03ce7274dbc521551e0abb42adef7b
Matheus Cristian <matheuscristianrg@gmail.com>
2025-06-09 19:00:47 -0300

    Conecta a interface com a API

--- api/src/main/java/com/example/api/controller/FuncionarioController.java
+++ api/src/main/java/com/example/api/controller/FuncionarioController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/funcionarios")
+@CrossOrigin("*")
 public class FuncionarioController {
 
     @Autowired
--- ui/src/cadastro.ts
+++ ui/src/cadastro.ts
@@ -28,7 +28,7 @@ if (
 
 async function doSearch() {
   async function showError(desc: string) {
-    await swal.fire("Erro", desc, "error");
+    await swal.fire("Erro!", desc, "error");
     searchButton.disabled = false;
     cep.disabled = false;
     cep.focus();
@@ -79,9 +79,58 @@ cep.addEventListener("input", () => {
   }
 });
 
-document.getElementById("form")?.addEventListener("submit", (e) => {
-  if (cep.value && document.activeElement === cep) {
+cep.addEventListener("keydown", (e) => {
+  if (e.key === "Enter" && cep.value && document.activeElement === cep) {
     e.preventDefault();
     doSearch();
+    return;
   }
+});
+
+document.getElementById("form")?.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const form = e.target as HTMLFormElement;
+
+  const nameInput = form.elements.namedItem("name") as HTMLInputElement;
+  const emailInput = form.elements.namedItem("email") as HTMLInputElement;
+  const passwordInput = form.elements.namedItem("password") as HTMLInputElement;
+  const cepInput = form.elements.namedItem("cep") as HTMLInputElement;
+  const addressInput = form.elements.namedItem("address") as HTMLInputElement;
+  const numberInput = form.elements.namedItem("number") as HTMLInputElement;
+  const neighborhoodInput = form.elements.namedItem(
+    "neighborhood"
+  ) as HTMLInputElement;
+  const cityInput = form.elements.namedItem("city") as HTMLInputElement;
+  const stateInput = form.elements.namedItem("state") as HTMLInputElement;
+
+  const data = {
+    nome: nameInput.value,
+    email: emailInput.value,
+    senha: passwordInput.value,
+    cep: cepInput.value,
+    endereco: addressInput.value,
+    numero: numberInput.value,
+    bairro: neighborhoodInput.value,
+    cidade: cityInput.value,
+    estado: stateInput.value,
+  };
+
+  fetch("http://localhost:8080/api/funcionarios", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  })
+    .then((response) => {
+      if (response.ok) {
+        swal.fire("Sucesso!", "Cadastro feito com sucesso.", "success");
+        form.reset();
+      } else {
+        swal.fire("Erro!", "Não foi possível realizar o cadastro.", "error");
+      }
+    })
+    .catch((error) => {
+      console.error("Erro:", error);
+      swal.fire("Erro!", "Erro de rede ou servidor.", "error");
+    });
 });
